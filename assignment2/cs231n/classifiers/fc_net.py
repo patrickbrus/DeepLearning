fcb6ddf0d09640e5af9c3eec34b363ef72557840
patrickbrus <brus.patrick63@googlemail.com>
2019-03-24 18:46:22 +0100

    first ieration task fully connected network

--- assignment2/cs231n/classifiers/fc_net.py
+++ assignment2/cs231n/classifiers/fc_net.py
@@ -47,7 +47,10 @@ class TwoLayerNet(object):
         # and biases using the keys 'W1' and 'b1' and second layer                 #
         # weights and biases using the keys 'W2' and 'b2'.                         #
         ############################################################################
-        pass
+        self.params['W1'] = weight_scale * np.random.randn(input_dim, hidden_dim)
+        self.params['b1'] = np.zeros(hidden_dim)
+        self.params['W2'] = weight_scale * np.random.randn(hidden_dim, num_classes)
+        self.params['b2'] = np.zeros(num_classes)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -77,7 +80,13 @@ class TwoLayerNet(object):
         # TODO: Implement the forward pass for the two-layer net, computing the    #
         # class scores for X and storing them in the scores variable.              #
         ############################################################################
-        pass
+        W1 = self.params['W1']
+        W2 = self.params['W2']
+        b1 = self.params['b1']
+        b2 = self.params['b2']
+
+        hidden_out,_ = affine_relu_forward(X, W1, b1)
+        scores,_ = affine_forward(hidden_out, W2, b2)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -97,7 +106,19 @@ class TwoLayerNet(object):
         # automated tests, make sure that your L2 regularization includes a factor #
         # of 0.5 to simplify the expression for the gradient.                      #
         ############################################################################
-        pass
+        loss, dout = softmax_loss(scores, y)
+
+        grads['W2'] = dout.T.dot(hidden_out).T + self.reg * W2
+        grads['b2'] = np.sum(dout, axis=0)
+
+        # get gradient from hidden layer
+        dhidden = np.dot(dout, W2.T)
+        dhidden[hidden_out == 0] = 0 
+
+        grads['W1'] = np.dot(X.T, dhidden) + self.reg * W1
+        grads['b1'] = np.sum(dhidden, axis=0)
+
+        loss += 0.5 * self.reg * np.sum(W1 * W1) + 0.5 * self.reg * np.sum(W2 * W2)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
